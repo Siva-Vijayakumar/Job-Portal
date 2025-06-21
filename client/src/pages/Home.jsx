7a6f8b5e1122ee1c591b9189f063c4dcd7ecc3bd
Siva V <sivavijayakumar1222@gmail.com>
2025-06-21 19:51:23 +0530

    correction done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from '../components/NavBar.jsx'
 import Hero from '../components/Hero.jsx'
-import JobListings from '../components/jobListings.jsx'
+import JobListings from '../components/JobListings.jsx'
 import AppDownload from '../components/AppDownload.jsx'
 import Footer from '../components/Footer.jsx'
 
