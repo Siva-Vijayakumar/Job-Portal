218e04d0e47d663a537189164d1d5425db26905d
Siva V <sivavijayakumar1222@gmail.com>
2025-06-21 20:41:23 +0530

    changed the Applications page

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -56,7 +56,7 @@ const Applications = () => {
       <NavBar />
       <div className='container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10'>
         <h2 className='text-xl font-semibold'>Your Resume</h2>
-        <div className='flex gap-2 mb-6 mt-3'>
+        {/* <div className='flex gap-2 mb-6 mt-3'>
           {
             isEdit || userData && userData.resume === ""
             ?
@@ -78,7 +78,55 @@ const Applications = () => {
               </button>
             </div>
           }
-        </div>
+        </div> */}
+      <div className='flex gap-2 mb-6 mt-3'>
+      {
+        isEdit || userData?.resume === ""
+        ? (
+          <>
+            <label className='flex items-center cursor-pointer' htmlFor="resumeUpload">
+              <p className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2'>
+                {resume ? resume.name : "Select Resume"}
+              </p>
+              <input
+                id='resumeUpload'
+                onChange={e => setResume(e.target.files[0])}
+                type="file"
+                hidden
+                accept='application/pdf'
+              />
+              <img src={assets.profile_upload_icon} alt="upload icon" />
+            </label>
+            <button
+              onClick={updateResume}
+              className='bg-green-100 border border-green-400 rounded-lg px-4 py-2'
+            >
+              Save
+            </button>
+          </>
+        ) : userData?.resume ? (
+          <div className='flex gap-2'>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href={userData.resume}
+              className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg'
+            >
+              Resume
+            </a>
+            <button
+              onClick={() => setIsEdit(true)}
+              className='text-gray-500 border border-gray-300 rounded-lg px-4 py-2'
+            >
+              Edit
+            </button>
+          </div>
+        ) : (
+          <p className="text-gray-400">No resume uploaded</p>
+        )
+      }
+    </div>
+
         <h2 className='text-xl font-semibold mb-4'>Jobs Applied</h2>
         <table className='min-w-full bg-white border rounded-lg'>
           <thead>
@@ -93,14 +141,14 @@ const Applications = () => {
           <tbody>
             {userApplications.map((job, index) => true ? (
               <tr key={index}>
-                <td className='py-3 px-4 flex items-center gap-2 border-b'>
+                <td className='py-3 px-4 flex items-center gap-2'>
                   <img className='w-8 h-8' src={job.companyId.image} alt="" />
                   {job.companyId.name}
                 </td>
-                <td className='py-2 px-4 border-b'>{job.jobId.title}</td>
-                <td className='py-2 px-4 border-b max-sm:hidden'>{job.jobId.location}</td>
-                <td className='py-2 px-4 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
-                <td className='py-2 px-4 border-b'>
+                <td className='py-2 px-4'>{job.jobId.title}</td>
+                <td className='py-2 px-4 max-sm:hidden'>{job.jobId.location}</td>
+                <td className='py-2 px-4 max-sm:hidden'>{moment(job.date).format('ll')}</td>
+                <td className='py-2 px-4'>
                   <span className={`${job.status === 'Accepted' ? 'bg-green-100' : job.status === 'Rejected' ? 'bg-red-100' : 'bg-blue-100'} px-4 py-1.5 rounded`}>
                     {job.status}
                   </span>
